563fd8de527aa9cfa88f6a65546ace8dddccec25
Epigene <augusts.bautra@gmail.com>
2026-05-15 16:25:17 +0300

    [2026-05-15 bronze] setup for chopper doen, now efficient chopping

--- lib/controller.rb
+++ lib/controller.rb
@@ -9,13 +9,33 @@ Inventory = Struct.new(:plum, :lemon, :apple, :banana, :iron, :wood) do
   end
 end
 Cell = Struct.new(:x, :y, :worker, :tree)
-Tree = Struct.new(:type, :x, :y, :size, :health, :fruits, :cooldown) do
+Tree = Struct.new(:type, :x, :y, :size, :health, :fruits, :cooldown, :period) do
   def node
     @node ||= "#{x} #{y}"
   end
 
+  def type?(desired_type)
+    type == desired_type
+  end
+
   def fruit?
     fruits >= 1
+  end
+
+  def grown?
+    size == 4
+  end
+
+  #  # lower is better. Ideally 0 - if the worker is already there. Can be negative is several fruits
+  #
+  # @return Numeric
+  def turns_till_fruit(worker, path_to)
+    return -fruits if fruits.positive?
+
+    distance_penalty = path_to.size
+    cooldown_penalty = (4 - size) * period + cooldown
+
+    [distance_penalty, cooldown_penalty].max
   end
 end
 Worker = Struct.new(:id, :player, :x, :y,
@@ -86,46 +106,175 @@ class Controller
       end
     end
 
-    # Initial boosting
-    if my_workers.size == 1
+    # Initial boosting has one goal - be able to afford an excellent chopper worker.
+    # It consists of 3 subgoals:
+    #  1. Reach 17 lemons
+    #  2. 10 iron
+    #  3. 5 plums (easy)
+    if my_workers.size == 1 && @commands.none? { _1.start_with?("TRAIN") }
       # TODO, maybe check if a cheap helper can be afforded and useful, usually should be
 
-      lemon_production_near_camp_per_turn = nodes_within_3_of_camp.sum do |near_node|
-        cell = cells[near_node]
-        next 0 if cell.nil? || cell.tree.nil? || cell.tree.type != "LEMON" || cell.tree.size < 4
+      ensure_sufficient_lemon_growth if my_inventory.lemon < 17
+      return @commands.join("; ") if @commands.any?
 
-        wet_nodes.include?(near_node) ? (1/8.0) : (1/3.0)
-      end
+      ensure_sufficient_plum_growth if my_inventory.plum < 5
+      return @commands.join("; ") if @commands.any?
 
-      if lemon_production_near_camp_per_turn < (11/24.0) # one watered and one regular or 4 regular
-        wet_path = wet_nodes_within_3_of_camp.select { cells[_1].nil? || cells[_1].tree.nil? }
-          .map { shortest_path(my_camp.node, _1) }
-          .min_by { _1.size }
+      gather_iron if my_inventory.iron < 10
+      return @commands.join("; ") if @commands.any?
 
-        if wet_path
-          handle_lemon_planting_at_end_of(my_workers.first, wet_path)
+      gather_lemons(my_workers.first) if my_inventory.lemon < 17
+      return @commands.join("; ") if @commands.any?
 
-          return (result = @commands.join("; ")) == "" ? "WAIT" : result
-        end
-
-        regular_path = nodes_within_3_of_camp.select { cells[_1].nil? || cells[_1].tree.nil? }
-          .map { shortest_path(my_camp.node, _1) }
-          .min_by { _1.size }
-
-        if regular_path
-          handle_lemon_planting_at_end_of(my_workers.first, regular_path )
-
-          return (result = @commands.join("; ")) == "" ? "WAIT" : result
-        end
-      end
+      gather_plums(my_workers.first) if my_inventory.plum < 5
+      return @commands.join("; ") if @commands.any?
     end
-    # MULTI worker moneymaking
-    my_workers.each do |worker|
+
+    debug("== OK, time to get a chopper and start chopping!")
+    organize_chopping
+    organize_chopper_helpers
+
+    (result = @commands.join("; ")) == "" ? "WAIT" : result
+  end
+  # Each turn you can print any number of commands, separated by ;.
+  # MOVE id x y Move troll id to cell (x, y).
+  # HARVEST id Make troll id harvest on its current cell.
+  # PLANT id type Make troll id plant a type on its current cell: PLUM, LEMON, APPLE or BANANA.
+  # CHOP id Make troll id chop on its current cell.
+  # PICK id type Make troll id pick one type from the shack: PLUM, LEMON, APPLE or BANANA.
+  # DROP id Make troll id drop all carried items at the shack.
+  # TRAIN moveSpeed carryCapacity harvestPower chopPower | Train a new troll with the given attributes.
+  # MINE id Make troll id mine a nearby IRON.
+  # WAIT to do nothing.
+  # MSG text to display a message in the replay.
+
+  private
+
+  def ensure_sufficient_lemon_growth
+    expected_lemon_production_near_camp_per_turn = nodes_within_3_of_camp.sum do |near_node|
+      cell = cells[near_node]
+      next 0 if cell.nil? || cell.tree.nil? || cell.tree.type != "LEMON"
+
+      wet_nodes.include?(near_node) ? (1/8.0) : (1/3.0)
+    end
+    debug "== Eventual Lemon production near camp per turn #{expected_lemon_production_near_camp_per_turn}"
+
+    return if expected_lemon_production_near_camp_per_turn >= (11/24.0) # 0.45; one watered and one regular or 4 regular
+
+    wet_path = wet_nodes_within_3_of_camp.select { cells[_1].nil? || cells[_1].tree.nil? }
+      .map { shortest_path(my_camp.node, _1) }
+      .min_by { _1.size }
+
+    if wet_path
+      handle_planting_at_end_of(my_workers.first, wet_path, "LEMON")
+      return
+    end
+
+    regular_path, _ = nodes_within_3_of_camp.select { cells[_1].nil? || cells[_1].tree.nil? }
+      .map { [shortest_path(my_camp.node, _1), shortest_path(my_workers.first.node, _1)] }
+      .min_by { _1.size + _2.size }
+
+    if regular_path
+      handle_planting_at_end_of(my_workers.first, regular_path, "LEMON")
+      return
+    end
+  end
+
+  def ensure_sufficient_plum_growth
+    expected_plum_production_near_camp_per_turn = nodes_within_3_of_camp.sum do |near_node|
+      cell = cells[near_node]
+      next 0 if cell.nil? || cell.tree.nil? || cell.tree.type != "PLUM"
+
+      wet_nodes.include?(near_node) ? (1/8.0) : (1/3.0)
+    end
+    debug "== Eventual Plum production near camp per turn #{expected_plum_production_near_camp_per_turn}"
+
+    return if expected_plum_production_near_camp_per_turn >= (1/8.0) # one tree on any wetness is sufficient
+
+    wet_path = wet_nodes_within_3_of_camp.select { cells[_1].nil? || cells[_1].tree.nil? }
+      .map { shortest_path(my_camp.node, _1) }
+      .min_by { _1.size }
+
+    if wet_path
+      handle_planting_at_end_of(my_workers.first, wet_path, "PLUM")
+      return
+    end
+
+    regular_path, _ = nodes_within_3_of_camp.select { cells[_1].nil? || cells[_1].tree.nil? }
+      .map { [shortest_path(my_camp.node, _1), shortest_path(my_workers.first.node, _1)] }
+      .min_by { _1.size + _2.size }
+
+    if regular_path
+      handle_planting_at_end_of(my_workers.first, regular_path, "PLUM")
+      return
+    end
+  end
+
+  def handle_planting_at_end_of(worker, path, tree_type)
+    getter = "carry_#{tree_type.downcase}"
+
+    if worker.node == path.last && worker.send(getter).positive?
+      @commands << "PLANT #{worker.id} #{tree_type}"
+    elsif worker.send(getter).positive?
+      @commands << "MOVE #{worker.id} #{path.last}"
+    elsif worker.node == path[1] && worker.send(getter).zero?
+      @commands << "PICK #{worker.id} #{tree_type}"
+    else # as in no lemon in hand, go to near camp
+      @commands << "MOVE #{worker.id} #{path[1]}"
+    end
+
+    nil
+  end
+
+  def gather_iron
+    # noop for now!
+  end
+
+  def gather_lemons(worker)
+    if worker.full? && grid.n4(my_camp.node).include?(worker.node)
+      @commands << "DROP #{worker.id}"
+    elsif worker.full? # go to dropoff
+      home_paths = grid.n4(my_camp.node).map { [_1, shortest_path(worker.node, _1)]}
+      next_to_home_node, path = home_paths.sort_by { |_node, path| path.size }.first
+
+      @commands << "MOVE #{worker.id} #{next_to_home_node}"
+    elsif cells[worker.node]&.tree&.type == "LEMON" && cells[worker.node]&.tree&.fruit? # at a tree already!
+      @commands << "HARVEST #{worker.id}"
+    else # gotta detect and go to a good candidate tree
+      path_to_tree, _turns_till = nodes_within_3_of_camp
+        .select { cells[_1]&.tree && cells[_1].tree.type?("LEMON") }
+        .map do |node|
+          path = shortest_path(worker.node, node)
+          [path, cells[node].tree.turns_till_fruit(worker, path)]
+        end.min_by { |_path, turns_till| turns_till }
+
+      @commands << "MOVE #{worker.id} #{path_to_tree.last}"
+    end
+  end
+
+  def gather_plums(worker)
+    # TODO
+  end
+
+  def organize_chopping
+    chopper = my_workers.max_by(&:carry_capacity)
+
+    path_to_wet_choppable = nodes_within_3_of_camp
+      .select { cells[_1]&.tree && cells[_1].tree.grown? && wet_nodes.include?(_1) }
+
+    path_to_dry_choppable = nodes_within_3_of_camp
+      .select { cells[_1]&.tree && wet_nodes.include?(_1) }
+  end
+
+  def organize_chopper_helpers
+    servant = my_workers.min_by(&:carry_capacity)
+
+    [servant].each do |worker|
       if worker.carry_banana > 0 && grid.n4(my_camp.node).include?(worker.node) && cells[worker.node].tree.nil?
         @commands << "PLANT #{worker.id} BANANA"
       elsif worker.full?
         home_paths = grid.n4(my_camp.node).map { [_1, shortest_path(worker.node, _1)]}
-        next_to_home_node, path = home_paths.sort_by { |node, path| grid.path_length(path) }.first
+        next_to_home_node, path = home_paths.sort_by { |node, path| path.size }.first
 
         if next_to_home_node && path.size == 1 # let's deposit!
           @commands << "DROP #{worker.id}"
@@ -149,40 +298,11 @@ class Controller
         end
       end
     end
-
-    (result = @commands.join("; ")) == "" ? "WAIT" : result
-  end
-  # Each turn you can print any number of commands, separated by ;.
-  # MOVE id x y Move troll id to cell (x, y).
-  # HARVEST id Make troll id harvest on its current cell.
-  # PLANT id type Make troll id plant a type on its current cell: PLUM, LEMON, APPLE or BANANA.
-  # CHOP id Make troll id chop on its current cell.
-  # PICK id type Make troll id pick one type from the shack: PLUM, LEMON, APPLE or BANANA.
-  # DROP id Make troll id drop all carried items at the shack.
-  # TRAIN moveSpeed carryCapacity harvestPower chopPower | Train a new troll with the given attributes.
-  # MINE id Make troll id mine a nearby IRON.
-  # WAIT to do nothing.
-  # MSG text to display a message in the replay.
-
-  private
-
-  def handle_lemon_planting_at_end_of(worker, path)
-    if worker.node == path.last && worker.carry_lemon.positive?
-      @commands << "PLANT #{worker.id} LEMON"
-    elsif worker.carry_lemon.positive?
-      @commands << "MOVE #{worker.id} #{path.last}"
-    elsif worker.node == path[1] && worker.carry_lemon.zero?
-      @commands << "PICK #{worker.id} LEMON"
-    else # as in no lemon in hand, go to near camp
-      @commands << "MOVE #{worker.id} #{path[1]}"
-    end
-
-    nil
   end
 
   # @return Numeric
   def fruit_tree_score(worker, tree, path)
-    distance = grid.path_length(path)
+    distance = path.size
 
     score = 0
     score -= distance * 100
@@ -211,7 +331,7 @@ class Controller
 
   def best_worker_cost
     # TRAIN moveSpeed carryCapacity harvestPower chopPower
-    # "TRAIN 2 4 0 3"
+    # "TRAIN 2 4 0 3" # 5, 17, 0, 10 costs
 
     existing_workers = my_workers.size
 
@@ -227,6 +347,13 @@ class Controller
     @my_workers[turn] = workers.select(&:my?)
   end
 
+  def tree_period_mapping
+    @tree_period_mapping ||= {
+      wet: {"PLUM" => 8, "LEMON" => 8, "APPLE" => 9, "BANANA" => 6},
+      dry: {"PLUM" => 3, "LEMON" => 3, "APPLE" => 2, "BANANA" => 4}
+    }
+  end
+
   def init_turn_variables!
     lines = input.split("\n")
 
@@ -238,7 +365,9 @@ class Controller
     @trees = []
     lines.shift.to_i.times do
       type, x, y, size, health, fruits, cooldown = lines.shift.split.map { _1[0].match?(%r'\d') ? _1.to_i : _1 }
-      tree = Tree.new(type, x, y, size, health, fruits, cooldown)
+
+      period = water_nodes.include?("#{x} #{y}") ? tree_period_mapping.dig(:wet, type) : tree_period_mapping.dig(:dry, type)
+      tree = Tree.new(type, x, y, size, health, fruits, cooldown, period)
       @trees << tree
       @cells["#{x} #{y}"] ||= Cell.new(x, y, nil, nil)
       @cells["#{x} #{y}"].tree = tree
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -39,100 +39,6 @@ RSpec.describe Controller, instance_name: :controller do
     let(:options) { {turn: turn, input: input} }
     let(:turn) { 1 }
 
-    context "when initialized with seed -5938658670755382000" do
-      let(:field) do
-        <<~FIELD
-          ................
-          .............1..
-          ................
-          ................
-          ................
-          ................
-          ..0.............
-          ................
-        FIELD
-      end
-
-      let(:input) do
-        <<~INPUT
-          0 0 0 0 0 0
-          0 0 0 0 0 0
-          18
-          PLUM 2 7 4 12 1 6
-          PLUM 13 0 4 12 1 6
-          PLUM 4 5 4 12 1 5
-          PLUM 11 2 4 12 1 5
-          PLUM 4 2 3 10 0 3
-          PLUM 11 5 3 10 0 3
-          LEMON 0 2 3 10 0 1
-          LEMON 15 5 3 10 0 1
-          APPLE 13 5 2 14 0 9
-          APPLE 2 2 2 14 0 9
-          APPLE 12 3 4 20 0 5
-          APPLE 3 4 4 20 0 5
-          APPLE 0 4 4 20 1 3
-          APPLE 15 3 4 20 1 3
-          BANANA 2 5 4 6 0 4
-          BANANA 13 2 4 6 0 4
-          BANANA 6 6 3 5 0 2
-          BANANA 9 1 3 5 0 2
-          2
-          0 0 2 6 1 1 1 0 0 0 0 0 0 0
-          1 1 13 1 1 1 1 0 0 0 0 0 0 0
-        INPUT
-      end
-
-      it "returns a move to go harvest the closest fruit" do
-        is_expected.to eq("MOVE 0 2 7")
-      end
-    end
-
-    context "when initialized with seed -6547937984084192000 | wood 1 planting" do
-      let(:field) do
-        <<~FIELD
-          ................
-          .......0........
-          ................
-          ................
-          ................
-          ................
-          ........1.......
-          ................
-        FIELD
-      end
-
-      let(:input) do
-        <<~INPUT
-          5 7 10 9 0 0
-          5 7 10 9 0 0
-          16
-          PLUM 0 7 2 8 0 8
-          PLUM 15 0 2 8 0 8
-          LEMON 9 1 4 12 2 5
-          LEMON 6 6 4 12 2 5
-          APPLE 9 2 3 17 0 8
-          APPLE 6 5 3 17 0 8
-          APPLE 14 1 4 20 0 4
-          APPLE 1 6 4 20 0 4
-          APPLE 0 5 4 20 2 8
-          APPLE 15 2 4 20 2 8
-          BANANA 11 0 4 6 3 5
-          BANANA 4 7 4 6 3 5
-          BANANA 9 5 4 6 2 3
-          BANANA 6 2 4 6 2 3
-          BANANA 12 1 3 5 0 4
-          BANANA 3 6 3 5 0 4
-          2
-          0 0 7 1 1 1 1 0 0 0 0 0 0 0
-          1 1 8 6 1 1 1 0 0 0 0 0 0 0
-        INPUT
-      end
-
-      it "returns a move to go harvest the closest high-yield fruit (banana, not lemon) and train a new worker" do
-        is_expected.to eq("TRAIN 1 1 1 0; MOVE 0 6 2")
-      end
-    end
-
     context "when initialized with seed=6972392475458301000 | Bronze lemon access" do
       let(:field) do
         <<~FIELD
@@ -185,6 +91,189 @@ RSpec.describe Controller, instance_name: :controller do
 
       it "returns a command to move to an open space near camp in prep for lemon pick" do
         is_expected.to eq("MOVE 0 8 8")
+      end
+    end
+
+    context "when initialized with seed=6972392475458301000 | Bronze lemon access, plant another" do
+      let(:field) do
+        <<~FIELD
+          ~#...~~~..........
+          ~~~..~~~.1..#.....
+          ~~...~~.#.#.......
+          .~~.............+.
+          ...#.#..##..#.#...
+          .+.............~~.
+          .......#.#.~~...~~
+          .....#..0.~~~..~~~
+          ..........~~~...#~
+        FIELD
+      end
+
+      let(:turn) { 4 }
+
+      let(:input) do
+        <<~INPUT
+          3 6 4 4 10 0
+          3 7 4 4 10 0
+          26
+          PLUM 8 1 4 12 1 3
+          PLUM 9 7 4 12 2 3
+          PLUM 6 8 4 12 0 1
+          PLUM 11 0 4 12 0 1
+          PLUM 14 3 3 10 0 5
+          PLUM 3 5 3 10 0 5
+          LEMON 4 4 2 8 0 3
+          LEMON 13 4 2 8 0 3
+          LEMON 14 5 4 12 0 3
+          LEMON 3 3 4 12 0 3
+          LEMON 10 1 2 8 0 3
+          LEMON 7 7 2 8 0 3
+          APPLE 15 0 4 20 0 2
+          APPLE 2 8 4 20 0 2
+          APPLE 10 4 4 20 2 4
+          APPLE 7 4 4 20 2 4
+          APPLE 17 2 3 17 0 8
+          APPLE 0 6 3 17 0 8
+          BANANA 17 5 3 5 0 3
+          BANANA 0 3 3 5 0 3
+          BANANA 4 0 2 4 0 2
+          BANANA 13 8 2 4 0 2
+          BANANA 17 1 4 6 1 6
+          BANANA 0 7 4 6 1 6
+          PLUM 8 0 1 6 0 3
+          LEMON 9 8 1 6 0 3
+          2
+          0 0 9 8 1 1 1 1 0 0 0 0 0 0
+          1 1 8 0 1 1 1 1 0 0 0 0 0 0
+        INPUT
+      end
+
+      it "returns a command to move to an open space near camp in prep for lemon pick" do
+        is_expected.to eq("MOVE 0 8 8")
+      end
+    end
+
+    context "when initialized with seed=6972392475458301000 | Bronze lemon access, gather initial lemons" do
+      let(:field) do
+        <<~FIELD
+          ~#...~~~..........
+          ~~~..~~~.1..#.....
+          ~~...~~.#.#.......
+          .~~.............+.
+          ...#.#..##..#.#...
+          .+.............~~.
+          .......#.#.~~...~~
+          .....#..0.~~~..~~~
+          ..........~~~...#~
+        FIELD
+      end
+
+      let(:turn) { 4 }
+
+      let(:input) do
+        <<~INPUT
+          8 6 4 4 10 0
+          3 6 3 4 10 0
+          27
+          PLUM 8 1 4 12 3 0
+          PLUM 9 7 4 12 0 1
+          PLUM 6 8 4 12 2 6
+          PLUM 11 0 4 12 2 6
+          PLUM 14 3 4 12 0 2
+          PLUM 3 5 4 12 0 2
+          LEMON 4 4 4 12 0 8
+          LEMON 13 4 4 12 0 8
+          LEMON 14 5 4 12 3 1
+          LEMON 3 3 4 12 3 1
+          LEMON 10 1 4 12 0 8
+          LEMON 7 7 4 12 0 8
+          APPLE 15 0 4 20 2 9
+          APPLE 2 8 4 20 2 9
+          APPLE 10 4 4 20 3 2
+          APPLE 7 4 4 20 3 2
+          APPLE 17 2 4 20 0 6
+          APPLE 0 6 4 20 0 6
+          BANANA 17 5 4 6 2 4
+          BANANA 0 3 4 6 2 4
+          BANANA 4 0 4 6 1 3
+          BANANA 13 8 4 6 1 3
+          BANANA 17 1 4 6 2 1
+          BANANA 0 7 4 6 2 1
+          PLUM 8 0 4 12 0 1
+          LEMON 9 8 4 12 0 1
+          LEMON 10 0 1 6 0 1
+          2
+          0 0 9 7 1 1 1 1 0 0 0 0 0 0
+          1 1 11 3 1 1 1 1 0 0 1 0 0 0
+        INPUT
+      end
+
+      it "returns a command to move to a nearby about-to-produce lemon tree" do
+        is_expected.to eq("MOVE 0 9 8")
+      end
+    end
+
+    context "when initialized with seed=6972392475458301000 | Just trained a chopper" do
+      let(:field) do
+        <<~FIELD
+          ~#...~~~..........
+          ~~~..~~~.1..#.....
+          ~~...~~.#.#.......
+          .~~.............+.
+          ...#.#..##..#.#...
+          .+.............~~.
+          .......#.#.~~...~~
+          .....#..0.~~~..~~~
+          ..........~~~...#~
+        FIELD
+      end
+
+      let(:turn) { 64 }
+
+      let(:input) do
+        <<~INPUT
+          0 0 3 4 0 0
+          4 0 3 4 2 0
+          28
+          PLUM 8 1 4 12 2 1
+          PLUM 9 7 4 12 2 2
+          PLUM 6 8 4 12 3 0
+          PLUM 11 0 4 12 3 0
+          PLUM 14 3 4 12 3 0
+          PLUM 3 5 4 12 3 0
+          LEMON 4 4 4 12 3 0
+          LEMON 13 4 4 12 3 0
+          LEMON 14 5 4 12 3 0
+          LEMON 3 3 4 12 3 0
+          LEMON 10 1 4 12 2 7
+          LEMON 7 7 4 12 3 0
+          APPLE 15 0 4 20 3 0
+          APPLE 2 8 4 20 3 0
+          APPLE 10 4 4 20 0 3
+          APPLE 7 4 4 20 3 0
+          APPLE 17 2 4 20 3 0
+          APPLE 0 6 4 20 3 0
+          BANANA 17 5 4 6 3 0
+          BANANA 0 3 4 6 3 0
+          BANANA 4 0 4 6 3 0
+          BANANA 13 8 4 6 3 0
+          BANANA 17 1 4 6 3 0
+          BANANA 0 7 4 6 3 0
+          PLUM 8 0 4 12 3 0
+          LEMON 9 8 4 12 3 0
+          LEMON 10 0 4 12 3 8
+          APPLE 9 2 4 20 1 9
+          5
+          0 0 9 7 1 1 1 1 1 0 0 0 0 0
+          1 1 10 3 1 1 1 1 0 0 1 0 0 0
+          2 1 7 4 2 2 1 2 0 0 0 0 0 0
+          3 1 9 0 1 2 1 1 0 0 0 0 0 0
+          4 0 8 7 2 4 0 3 0 0 0 0 0 0
+        INPUT
+      end
+
+      it "returns commands to chop the juiciest nearby spot for future bananas, and for helper to start planting bananas" do
+        is_expected.to eq("MOVE 4 9 8; DROP 0") # since helper 0 is trapped, remain to work where one is
       end
     end
 
